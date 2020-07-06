21d6cb86277f8739ff797ff309a049cd9460e9bd
InQueueq <dimadorko@gmail.com>
2020-07-06 15:47:51 +0300

    remake scraper using mongo driver and webhook

--- scraper.js
+++ scraper.js
@@ -1,74 +1,88 @@
-const mongoose = require('mongoose')
-const URI = require('./config.js').URI
-mongoose.connect(URI,{useNewUrlParser:true,useUnifiedTopology: true},);
+require('dotenv/config')
+const TOKEN = process.env.TOKEN
+const URL = process.env.URL
+const URI = process.env.URI
+const MongoClient = require('mongodb').MongoClient;
+const dbName = 'rss';
 const Parser = require("rss-parser");
+const parser = new Parser();
 const CronJob = require('cron').CronJob;
-const Article = require('./articles')
 const stopword = require('stopword')
 const express = require('express')
-const validator = require('validator')
 const app = express()
 const PORT = 5000 || process.env.PORT;
 const paginationLimit = 20;
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = require('./config.js').TOKEN
-const id = [];
+const client = new MongoClient(URI);
 
-const parser = new Parser();
-
-
-const bot = new TelegramBot(TOKEN,{
-    polling: true
-})
-bot.onText(/\/start/, function (msg, match) {
-    id.push(msg.from.id);
-    bot.sendMessage(msg.from.id,`Hello ${msg.from.first_name}, welcome!`);
+const bot = new TelegramBot(TOKEN, {
+    webHook:{
+        port: PORT,
+    }
 });
 
-// Call it once in constant N mins
-// Save posts to MongoDB
-// API paginates 20 posts from DB
-//
+bot.setWebHook(`${URL}/bot${TOKEN}`);
 
-app.get('/articles',async (req, res)=>{
-    const results = {};
-    const escapeQuery = Object.assign({}, ...Object.keys(req.query).map(key=>{
-        return {[key]:validator.escape(req.query[key])}
-    }))
-    const page = parseInt(escapeQuery.page) || 1;
-    const filter = escapeQuery.filter || ""
-    const filterQuery = {
-        keywords: new RegExp(filter, 'i')
-    }
-    const startIndex = (page - 1) * paginationLimit;
-    results.results = await Article.find(filterQuery).limit(paginationLimit).skip(startIndex).exec();
-    res.json(results);
-})
+app.use(express.json());
 
 const job = new CronJob({
     cronTime: '0 */5 * * * *',
     onTick: async function() {
-        const articles = mongoose.connection.db.collection('articles');
-        const feed = await parser.parseURL("https://news.ycombinator.com/rss").catch(err=>{
-            console.log("Unable to parse")
-        });
-        const maxPublishingDate = await articles.find().count() <= 0 ? new Date(1991,11) : (await articles.find().sort({pubDate:-1}).limit(1).toArray())[0].pubDate;
+        const feed = await parser.parseURL("https://news.ycombinator.com/rss")
+        const item = (await this.articles.find().sort({pubDate:-1}).limit(1).toArray())[0];
+        const maxPublishingDate = item ? item.pubDate : new Date(0);
         const filteredArticles = feed.items.filter(item=>new Date(item.pubDate) > maxPublishingDate);
-        filteredArticles.map(item=>{
-            id.forEach(userId=>{
-                bot.sendMessage(userId,`New post is out! \n${item.title}\n${item.link}`)
+        filteredArticles.forEach(item=>{
+            item.keywords = stopword.removeStopwords(item.title.split(' ')).join(' ');
+            item.pubDate = new Date(item.pubDate);
+        })
+        if(filteredArticles[0]) this.articles.insertMany(filteredArticles);
+        const users = await this.users.find().toArray()
+        users.forEach(user=>{
+            filteredArticles.forEach(item=>{
+                bot.sendMessage(user.id,`New post is out! \n${item.title}\n${item.link}`)
             })
-            const article = new Article(item);
-            article.keywords = stopword.removeStopwords(article.title.split(' ')).join(' ');
-            article.save()
-                .then(item => {console.log("The item has been to database");
-                })
-                .catch(err=>{console.log(err);
-                });
         })
     }
 });
-mongoose.connection.on('open',function(err,db) {
+
+const main = async() => {
+    await client.connect();
+    const db = client.db(dbName);
+    const articles = db.collection('articles');
+    const users = db.collection('users');
+    job.articles = articles
+    job.users = users;
     job.start();
-});
-app.listen(PORT, ()=> console.log(`Server started on ${PORT} port`));
+
+    app.get('/articles', async (req, res)=>{
+        const results = {};
+        const page = parseInt(req.query.page) || 1;
+        const filter = req.query.filter || ""
+        const filterQuery = {
+            keywords: new RegExp(filter, 'i')
+        }
+        const startIndex = (page - 1) * paginationLimit;
+        results.results = await articles.find(filterQuery).limit(paginationLimit).skip(startIndex).toArray();
+        res.json(results);
+    })
+
+    app.post(`/bot${TOKEN}`, (req, res) => {
+        bot.processUpdate(req.body);
+        res.sendStatus(200);
+    });
+
+    app.listen(PORT, ()=> console.log(`Server started on ${PORT} port`));
+
+    bot.onText(/\/start/, function (msg) {
+        users.findOne(msg.from).then(user=>{
+            bot.sendMessage(user.id,`Hello ${user.first_name}, welcome!`);
+        }).catch(user=>{
+            users.insertOne(msg.from);
+            bot.sendMessage(user.id,`Hello ${user.first_name}, welcome!`);
+        });
+    });
+
+}
+
+main().catch(err=>{console.log(err)});
